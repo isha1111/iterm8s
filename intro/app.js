d6be65d6a9d101b109c1584561540aa0c7bc40e3
Isha <isha.negi19@gmail.com>
2016-03-16 14:04:27 +1100

    commit js files

---
+++ intro/app.js
@@ -0,0 +1,6 @@
+var numberOfChildren ='N';
+var PartnerName = 'z';
+var Location = 'Y';
+var job = 'X';
+
+console.log(" I an Isha married to "+ PartnerName+ ",with children "+ numberOfChildren + "living in "+ Location +" working as " + job);
---
+++ intro/intro2.js
@@ -0,0 +1,40 @@
+console.log('hi I am in Intro2 js');
+
+if(5>4)
+{
+  console.log("math is pretty easy");
+}
+
+var iterm8sIsAwesome = false;
+
+if(iterm8sIsAwesome === true)
+{
+  console.log('yay iterm8s');
+}
+if(iterm8sIsAwesome)
+{
+  console.log('nothing to say');
+}
+var points = 0;
+if(points)
+{
+  console.log("you have "+points+ " points");
+}
+
+var firstname = null;
+if(firstname)
+{
+  console.log("you first name is "+firstname);
+}
+var lastname = undefined;
+if(lastname)
+{
+  cosnole.log("your lastname is "+ lastname);
+}
+
+//empty string false
+var fullname = '';
+if(fullname)
+{
+  console.log("yur lastname is "+ lastname);
+}
